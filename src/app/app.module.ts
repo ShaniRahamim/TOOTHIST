87860aed945e74bbb3c546ff2d444eb32f7fc3a3
shani <shani@hopon.co.il>
2017-05-03 10:08:19 +0300

    hello toothist

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
+import { ItemDetailsPage } from '../pages/item-details/item-details';
+import { ListPage } from '../pages/list/list';
+import { Users } from '../pages/users/users';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage,
+    Users
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage,
+    Users
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Users,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/users/users.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+/*import { IonicPage, NavController, NavParams } from 'ionic-angular';*/
+import {IonicPage} from 'ionic-angular';
+
+/**
+ * Generated class for the Users page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-users',
+  templateUrl: 'users.html',
+})
+export class Users {
+
+  /*constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }*/
+  constructor() {
+   }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Users');
+  }
+
+}
